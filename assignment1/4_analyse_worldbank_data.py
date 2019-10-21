d5cb07408a52946f9523a0792cfddd8ebbf443db
Vesperpiano <barry.j.a.smart@gmail.com>
2019-10-21 22:25:41 +0100

    Tidying up code and commentary.

--- assignment1/4_analyse_worldbank_data.py
+++ assignment1/4_analyse_worldbank_data.py
@@ -37,23 +37,27 @@ github_path = "C:/Users/Barry/"
 data_path = github_path + "GitHub/CS982/assignment1/"
 interpolated_data_set = pd.read_pickle(data_path + "interpolated_data_set.pkl")
 #%% [markdown]
-### Stage 7.2 - Add Additional Calculated Columns
-# This step was added retrospectively, because inspection of the data shows that we will be required to work with "Log GDP" in order to generate more meaningful analysis.
+### Stage 7.2 - Prepare Source Data for Analysis
+# A few simple steps to prepare the data set for visualisation:
+# - Add column : Log10 of GDP - This step was added retrospectively, because inspection of the data shows that we will be required to work with "Log GDP" in order to generate more meaningful analysis.
+# - Flatten the data by resetting the index so that the index columns are available for plotting.
+#
 
 #%%
 interpolated_data_set["Log GDP per Capita"] = np.log10(interpolated_data_set["GDP per capita (current US$)"])
-interpolated_data_set.head(10)
+interpolated_data_set_flattened = interpolated_data_set.reset_index()
 
-# Useful link for slicing multi indexes:
-# [https://pandas.pydata.org/pandas-docs/stable/user_guide/advanced.html#advanced](https://pandas.pydata.org/pandas-docs/stable/user_guide/advanced.html#advanced)
-#
-# df.loc[(slice('A1', 'A3'), ...), :]
+#%%
+mean_by_region_and_year = interpolated_data_set.groupby(level=["Region", "Year"]).mean().reset_index()
+mean_by_region_and_year.index.name = 'ID'
+
+#%%
+mean_by_country_and_decade = interpolated_data_set.groupby(level=["Region", "Country", "Decade"]).mean().reset_index()
+mean_by_country_and_decade.index.name = 'ID'
 
 #%% [markdown]
-### Stage 7.3 - Narrow Analysis To Data For 2018
-# In this section I will analyse data for 2018 in more depth with the purpose of:
-# - Generating some initial insights into the data;
-# - Trying to bring out the correlations between data;
+### Stage 7.3 - Create "Slice" Of Data For 2018
+# In this section I will analyse data for 2018 in more depth with the purpose of generating some simple initial insights into the data.
 #
 #%%
 # Use the xs function to grab a cross section of the data, using the power of the multi-level-index.
@@ -61,7 +65,6 @@ analysis_of_2018 = interpolated_data_set.xs(2018, level="Year", drop_level=False
 # Flatten the dataframe to open up all columns for access by the matplotlib and seaborn libraries.
 analysis_of_2018_flattened = analysis_of_2018.reset_index()
 analysis_of_2018_flattened.index.name = 'ID'
-analysis_of_2018_flattened.describe()
 
 #%% [markdown]
 ### Stage 7.4 - Analysis of Life Expectancy
@@ -87,16 +90,14 @@ analysis_of_2018_flattened.loc[:,["Country", "Region", "Life expectancy at birth
 # - The gap between the bottom and top is over 30 years!;
 # - The bottom 10 countries are **ALL** in Sub-Saharan Africa.
 #
-#Later I should look at how this gap has developed over time.
-#TODO add backlog item for this.
-# 
 #### Life Expectancy 2018 - Analysing Distribution of Data for All Countries
 #Now using box plots to look at distribution by country.
 
 #%%
+# Create a helper function for box plots of different columns by region.
 def region_box_plot(data_frame, x_column, plot_title, x_scale="linear"):
     sns.set_style("ticks", {'axes.grid': True, 'grid.color': '.8', 'grid.linestyle': '-'})
-    f, ax = plt.subplots(figsize=(10, 10))
+    f, ax = plt.subplots(figsize=(10, 5))
     ax.set(xscale=x_scale)
     plt.title(plot_title, fontdict = {"fontsize" : 20})
     box_plot = sns.boxplot(x=x_column, y="Region", order=region_ranking, data=analysis_of_2018_flattened, palette=region_palette, ax=ax)
@@ -109,6 +110,9 @@ region_box_plot(analysis_of_2018_flattened, "Life expectancy at birth, total (ye
 # There is a significant outlier for "Latin America & Caribbean" - it would be good to investigate this further.
 # TODO - add a backlog item.
 #
+# The country is Haiti:
+# (https://borgenproject.org/top-10-facts-about-life-expectancy-in-haiti/)[https://borgenproject.org/top-10-facts-about-life-expectancy-in-haiti/]
+# 
 
 #%%
 analysis_of_2018_flattened.loc[:,["Country", "Region", "Life expectancy at birth, total (years)"]]\
@@ -116,13 +120,10 @@ analysis_of_2018_flattened.loc[:,["Country", "Region", "Life expectancy at birth
     .nsmallest(10, "Life expectancy at birth, total (years)")
 
 #%%
-haiti_data = interpolated_data_set.reset_index()
-haiti_data = haiti_data.loc[haiti_data["Country"] == "Lithuania"]
+haiti_data = interpolated_data_set_flattened.loc[interpolated_data_set_flattened["Country"] == "Haiti"]
 sns.set_style("ticks", {'axes.grid': True, 'grid.color': '.8', 'grid.linestyle': '--'})
 f, ax = plt.subplots(figsize=(10, 10))
-# ax.set(yscale="log")
-#ax.set_ylim(-5, 10)
-plt.title("YYY", fontdict = {"fontsize" : 20})
+plt.title("Population of Haiti", fontdict = {"fontsize" : 20})
 sns.lineplot(x="Year", y="Population, total",\
     color="gray", data=haiti_data, ax=ax)
 
@@ -136,9 +137,6 @@ sns.lineplot(x="Year", y="Population, total",\
 # - Roll up the data by calculating the mean of *ALL* series by Year for each Region.
 # - Generage a line plot to show how each series develops over time.
 #
-#%%
-mean_by_region_and_year = interpolated_data_set.groupby(level=["Region", "Year"]).mean()
-mean_by_region_and_year
 
 #%%
 def region_line_plot(data_frame, y_column, plot_title, y_scale="linear"):
@@ -154,11 +152,7 @@ def region_line_plot(data_frame, y_column, plot_title, y_scale="linear"):
         data=data_frame, ax=ax)
 
 #%%
-region_line_plot(mean_by_region_and_year.reset_index(), "Life expectancy at birth, total (years)", "Development of Life Expectancy by Region\nby Year since 1960", y_scale="linear")
-
-#%%
-mean_by_country_and_decade = interpolated_data_set.groupby(level=["Region", "Country", "Decade"]).mean().reset_index()
-mean_by_country_and_decade
+region_line_plot(mean_by_region_and_year, "Life expectancy at birth, total (years)", "Development of Life Expectancy by Region\nby Year since 1960", y_scale="linear")
 
 
 #%%
@@ -175,7 +169,7 @@ sns.swarmplot(x="Decade", y="Life expectancy at birth, total (years)", hue="Regi
 # - Meanwhile life expectancy for the Sub-Saharan Africa region has not improved at the same rate, mainly as a result of a plateau in the 1990s;
 # - The net result is that the gap between those countries with the worst and best record for life expectancy has not closed appreciably since 1960.
 #
-#%%
+#%% [markdown]
 ### Stage 7.5 - Analysing Gross Domestic Product (GDP)
 #Using a number of techniques to get a feel for the life expectany data:
 # - Looking at top 10 and bottom 10 countries in 2018;
@@ -205,11 +199,7 @@ analysis_of_2018_flattened.loc[:,["Country", "Region", "GDP per capita (current 
 #Now using box plots to look at distribution by country.
 
 #%%
-sns.set_style("ticks", {'axes.grid': True, 'grid.color': '.8', 'grid.linestyle': '-'})
-f, ax = plt.subplots(figsize=(10, 10))
-plt.title("Distribution of Gross Domestic Product (GDP)\nby Region in 2018", fontdict = {"fontsize" : 20})
-box_plot = sns.boxplot(x="GDP per capita (current US$)",\
-    y="Region", order=region_ranking, data=analysis_of_2018_flattened, ax=ax)
+region_box_plot(analysis_of_2018_flattened, "GDP per capita (current US$)", "Distribution of Gross Domestic Product (GDP)\nby Region in 2018")
 
 #%% [markdown]
 #The data is quite skewed given that it is spread over 4 orders of magnitude.
@@ -217,12 +207,7 @@ box_plot = sns.boxplot(x="GDP per capita (current US$)",\
 #
 
 #%%
-sns.set_style("ticks", {'axes.grid': True, 'grid.color': '.8', 'grid.linestyle': '-'})
-f, ax = plt.subplots(figsize=(10, 10))
-ax.set(xscale="log")
-plt.title("Distribution of GDP by Region in 2018\nUsing Log10 Scale", fontdict = {"fontsize" : 20})
-box_plot = sns.boxplot(x="GDP per capita (current US$)",\
-    y="Region", order=region_ranking, data=analysis_of_2018_flattened, ax=ax)
+region_box_plot(analysis_of_2018_flattened, "GDP per capita (current US$)", "Distribution of Log10 of GDP\nby Region in 2018", x_scale="log")
 
 #%% [markdown]
 # It will be useful to create a new series of data in the dataframe that is the Log10 of GDP.
@@ -233,22 +218,15 @@ box_plot = sns.boxplot(x="GDP per capita (current US$)",\
 #Lets now examine how the gap in GDP across different nations has developed over time.
 #
 #%%
-sns.set(style="ticks")
-f, ax = plt.subplots(figsize=(10, 10))
-plt.title("Development of GDP by Region\nby Year Since 1960", fontdict = {"fontsize" : 20})
-sns.lineplot(x="Year", y="GDP per capita (current US$)",\
-    hue="Region",\
-    hue_order=region_ranking,\
-    linewidth=3,\
-    data=mean_by_region_and_year.reset_index(), ax=ax)
+region_line_plot(mean_by_region_and_year, "GDP per capita (current US$)", "Development of GDP Per Capita by Region\nby Year Since 1960", y_scale="log")
 
 #%%
 sns.set_style("ticks", {'axes.grid': True, 'grid.color': '.8', 'grid.linestyle': '-'})
 f, ax = plt.subplots(figsize=(10, 10))
-# ax.set(yscale="log")
-plt.title("YYY", fontdict = {"fontsize" : 20})
-sns.boxplot(x="Decade", color="gray",\
-    y="GDP per capita (current US$)", orient="v", data=interpolated_data_set.reset_index(), ax=ax)
+#ax.set(yscale="log")
+plt.title("Development of GDP by Country\nby Decade Since 1960", fontdict = {"fontsize" : 20})
+sns.swarmplot(x="Decade", y="GDP per capita (current US$)", hue="Region",\
+    palette=region_palette, data=mean_by_country_and_decade)
 
 #%% [markdown]
 #### Conclusions - Development of GDP Over Time
@@ -268,25 +246,25 @@ sns.boxplot(x="Decade", color="gray",\
 #%% [markdown]
 #### Population 2018 - Top 10 Countries
 #%%
-analysis_of_2018_flattened.loc[:,["Country", "Region", "Population, total", "Population growth (annual %)"]]\
+analysis_of_2018_flattened.loc[:,["Country", "Region", "Population, total"]]\
     .nlargest(10, "Population, total")
 #%% [markdown]
 #### Population 2018 - Bottom 10 Countries
 #%%
-analysis_of_2018_flattened.loc[:,["Country", "Region", "Population, total", "Population growth (annual %)"]]\
+analysis_of_2018_flattened.loc[:,["Country", "Region", "Population, total"]]\
     .nsmallest(10, "Population, total")
 
 #%% [markdown]
 #### Population Growth 2018 - Top 10 Countries
 #%%
-analysis_of_2018_flattened.loc[:,["Country", "Region", "Population, total", "Population growth (annual %)"]]\
+analysis_of_2018_flattened.loc[:,["Country", "Region", "Population growth (annual %)"]]\
     .nlargest(10, "Population growth (annual %)")
 
 #%% [markdown]
 #### Population Growth 2018 - Bottom 10 Countries
 
 #%%
-analysis_of_2018_flattened.loc[:,["Country", "Region", "Population, total", "Population growth (annual %)"]]\
+analysis_of_2018_flattened.loc[:,["Country", "Region", "Population growth (annual %)"]]\
     .nsmallest(10, "Population growth (annual %)")
 
 #%%
@@ -294,31 +272,14 @@ analysis_of_2018_flattened.loc[analysis_of_2018_flattened["Population growth (an
 
 
 #%%
-sns.set(style="ticks")
-f, ax = plt.subplots(figsize=(10, 10))
-sns.lineplot(x="Year", y="Population, total",\
-    hue="Region",\
-    hue_order=region_ranking,\
-    linewidth=3,\
-    data=mean_by_region_and_year.reset_index(), ax=ax)
-
-#%%
-sns.set(style="ticks")
-f, ax = plt.subplots(figsize=(10, 10))
-sns.lineplot(x="Year", y="Population growth (annual %)",\
-    hue="Region",\
-    hue_order=region_ranking,\
-    linewidth=3,\
-    data=mean_by_region_and_year.reset_index(), ax=ax)
+region_line_plot(mean_by_region_and_year, "Population, total", "Average Country Population by Region\nby Year Since 1960", y_scale="log")
 
 #%%
 sns.set_style("ticks", {'axes.grid': True, 'grid.color': '.8', 'grid.linestyle': '-'})
 f, ax = plt.subplots(figsize=(10, 10))
-# ax.set(yscale="log")
-ax.set_ylim(-5, 10)
-plt.title("YYY", fontdict = {"fontsize" : 20})
-sns.boxplot(x="Decade", y="Population growth (annual %)",\
-    color="gray", data=interpolated_data_set.reset_index(), ax=ax)
+plt.title("Population Growth by Country\nby Decade Since 1960", fontdict = {"fontsize" : 20})
+sns.swarmplot(x="Decade", y="Population growth (annual %)", hue="Region",\
+    palette=region_palette, data=mean_by_country_and_decade)
 
 
 
--- assignment1/5_analyse_relationships.py
+++ assignment1/5_analyse_relationships.py
@@ -19,6 +19,14 @@ import matplotlib.pyplot as plt
 import scipy
 import assignment1.data_wrangling_functions as dwf
 
+#%%
+region_ranking = ["North America", "Europe & Central Asia", "Middle East & North Africa",\
+    "Latin America & Caribbean", "East Asia & Pacific", "South Asia", "Sub-Saharan Africa"]
+region_palette = {"North America" : "red", "Europe & Central Asia" : "blue",\
+    "Middle East & North Africa" : "pink", "Latin America & Caribbean" : "purple",\
+        "East Asia & Pacific" : "green", "South Asia" : "orange", "Sub-Saharan Africa" : "gray"}
+
+
 #%% [markdown]
 ### Stage 7.1 - Read The File
 # Read in the file that was generated from the previous script.
@@ -33,46 +41,45 @@ interpolated_data_set = pd.read_pickle(data_path + "interpolated_data_set.pkl")
 
 #%%
 interpolated_data_set["Log GDP per Capita"] = np.log10(interpolated_data_set["GDP per capita (current US$)"])
+interpolated_data_set_flattened = interpolated_data_set.reset_index()
 
 #%%
-interpolated_data_set.head(10)
+mean_by_region_and_year = interpolated_data_set.groupby(level=["Region", "Year"]).mean().reset_index()
+mean_by_region_and_year.index.name = 'ID'
 
 #%%
-interpolated_data_set.columns
+mean_by_region_and_decade = interpolated_data_set.groupby(level=["Region", "Decade"]).mean().reset_index()
+mean_by_region_and_decade.index.name = 'ID'
+
+
+#%%
+mean_by_country_and_decade = interpolated_data_set.groupby(level=["Region", "Country", "Decade"]).mean().reset_index()
+mean_by_country_and_decade.index.name = 'ID'
+
+#%%
+# In this section I will analyse data for 2018 in more depth with the purpose of generating some simple initial insights into the data.
+# Use the xs function to grab a cross section of the data, using the power of the multi-level-index.
+analysis_of_2018 = interpolated_data_set.xs(2018, level="Year", drop_level=False)
+# Flatten the dataframe to open up all columns for access by the matplotlib and seaborn libraries.
+analysis_of_2018_flattened = analysis_of_2018.reset_index()
+analysis_of_2018_flattened.index.name = 'ID'
 
 #%% [markdown]
-### Stage 7.2 - ???
+### Stage 7.2 - Heat Map Analysis of Correlations
 # In this section I will analyse data for 2018 in more depth with the purpose of:
 # - Generating some initial insights into the data;
 # - Trying to bring out the correlations between data;
 #
 #First step is therefore to take a slice out of the data set.
 #
-#%%
-region_ranking = ["North America", "Europe & Central Asia", "Middle East & North Africa", "Latin America & Caribbean", "East Asia & Pacific", "South Asia", "Sub-Saharan Africa"]
-
-sns.set(style="ticks")
-f, ax = plt.subplots(figsize=(10, 10))
-plt.title("XXX", fontdict = {"fontsize" : 20})
-sns.scatterplot(x="Log GDP per Capita", y="Life expectancy at birth, total (years)",\
-    hue="Region",\
-    size="Population, total",\
-    #palette="ch:r=-.2,d=.3_r",\
-    hue_order=region_ranking,\
-    sizes=(40, 1000), linewidth=1,\
-    data=analysis_of_2018_flattened, ax=ax)
-
-#%%
-analysis_of_2018_flattened["Region"].value_counts()
 
 #%%
 # Compute the correlation matrix
-correlation_matrix = analysis_of_2018.corr()
+correlation_matrix = interpolated_data_set.corr()
 
-#%%
 # Generate a mask for the upper triangle
-mask = np.zeros_like(correlation_matrix, dtype=np.bool)
-mask[np.triu_indices_from(mask)] = True
+#mask = np.zeros_like(correlation_matrix, dtype=np.bool)
+#mask[np.triu_indices_from(mask)] = True
 
 sns.set(style="ticks")
 
@@ -80,105 +87,20 @@ sns.set(style="ticks")
 f, ax = plt.subplots(figsize=(11, 9))
 
 # Generate a custom diverging colormap
-cmap = sns.diverging_palette(220, 10, as_cmap=True)
+cmap = sns.diverging_palette(10, 220, as_cmap=True)
 
 # Draw the heatmap with the mask and correct aspect ratio
-sns.heatmap(correlation_matrix, mask=mask,\
+sns.heatmap(correlation_matrix,\
+    #mask=mask,\
     cmap=cmap,\
-    vmax=.3, center=0,\
+    vmax=1, center=0,\
     square=True, linewidths=.5, cbar_kws={"shrink": .5})
 
-#%%
-mean_by_region = analysis_of_2018.groupby(level="Region").mean()
 
-#%%
-sns.set(style="ticks")
-f, ax = plt.subplots(figsize=(10, 10))
-sns.scatterplot(x="Log GDP per Capita", y="Life expectancy at birth, total (years)",\
-    hue="Region",\
-    #size="depth",\
-    #palette="ch:r=-.2,d=.3_r",\
-    hue_order=region_ranking,\
-    sizes=(1, 8), linewidth=0,\
-    data=mean_by_region.reset_index(), ax=ax)
-
-
-#%%
-mean_by_region_and_decade = interpolated_data_set.groupby(level=["Region", "Decade"]).mean()
-
-#%%
-mean_by_region_and_decade
-
-#%%
-sns.set(style="ticks")
-f, ax = plt.subplots(figsize=(10, 10))
-sns.lineplot(x="Decade", y="Life expectancy at birth, total (years)",\
-    hue="Region",\
-    #size="depth",\
-    #palette="ch:r=-.2,d=.3_r",\
-    hue_order=region_ranking,\
-    sizes=(1, 8), linewidth=3,\
-    data=mean_by_region_and_decade.reset_index(), ax=ax)
-
-
-
-#%%
-sns.set(style="ticks")
-f, ax = plt.subplots(figsize=(10, 10))
-sns.lineplot(x="Year", y="Population growth (annual %)",\
-    hue="Region",\
-    #size="depth",\
-    #palette="ch:r=-.2,d=.3_r",\
-    hue_order=region_ranking,\
-    sizes=(1, 8), linewidth=3,\
-    data=mean_by_region_and_year.reset_index(), ax=ax)
-
-#%%
-sns.set(style="ticks")
-f, ax = plt.subplots(figsize=(10, 10))
-sns.lineplot(x="Year", y="Population, total",\
-    hue="Region",\
-    #size="depth",\
-    #palette="ch:r=-.2,d=.3_r",\
-    hue_order=region_ranking,\
-    sizes=(1, 8), linewidth=3,\
-    data=mean_by_region_and_year.reset_index(), ax=ax)
-
-#%%
-sns.set(style="ticks")
-f, ax = plt.subplots(figsize=(20, 15))
-region_ranking = ["North America", "Europe & Central Asia", "Middle East & North Africa", "Latin America & Caribbean", "East Asia & Pacific", "South Asia", "Sub-Saharan Africa"]
-sns.boxplot(x="Decade", y="Population growth (annual %)",\
-    hue="Region", data=interpolated_data_set.reset_index(), ax=ax)
-
-#%%
-max = analysis_of_2018_flattened["Population, total"].max()
-
-#%%
-min = analysis_of_2018_flattened["Population, total"].min()
-
-
-#%%
-import math
-def custom_round(x, up=True):
-    if up:
-        return math.pow(10, math.ceil(math.log10(x)))
-    else:
-        return math.pow(10, math.floor(math.log10(x)))
-
-#%%
-custom_round(max)
-
-#%%
-#%%
-custom_round(min, up=False)
-
-#%%
 
 #%%
 min_power, min_value, max_power, max_value = dwf.find_min_and_max(analysis_of_2018_flattened, "GDP per capita (current US$)")
 
-#%%
 #%%
 sns.set_style("ticks", {'axes.grid': True, 'grid.color': '.8', 'grid.linestyle': '-'})
 f, ax = plt.subplots(figsize=(10, 10))
@@ -224,9 +146,11 @@ sns.lmplot(x="Log GDP per Capita", y="Life expectancy at birth, total (years)",\
     height=8, data=analysis_of_2018_flattened)
 
 #%%
-#%%
 sns.set_style("ticks", {'axes.grid': True, 'grid.color': '.8', 'grid.linestyle': '-'})
 sns.lmplot(x="Log GDP per Capita", y="Population growth (annual %)",\
     height=8, data=analysis_of_2018_flattened)
 
 
+
+
+#%%
