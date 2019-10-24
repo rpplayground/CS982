e246adc3d15ee1a4e9b0eb687eef26a9e280d4e2
Vesperpiano <barry.j.a.smart@gmail.com>
2019-10-24 13:52:58 +0100

    Lab 4 ongoing confustion.

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -12,39 +12,26 @@ from sklearn import datasets
 from sklearn import metrics
 from sklearn import cluster
 from sklearn.preprocessing import scale
+from sklearn.preprocessing import LabelEncoder
 
 #%% [markdown]
 # 2. Import the Balance Scale dataset available at
 # http://archive.ics.uci.edu/ml/datasets/balance+scale
 
 #%%
-dataset = pd.read_csv("./lab4/balance-scale.data")
+dataset = pd.read_csv("./lab4/balance-scale.data", header=None)
 dataset.head(10)
 
 #%%
-labels = dataset.values[:,0]
-data = dataset.values[:,1:5]
+# NOTE - this step should not be required!
+# This drops the first row from the data set.
+dataset = dataset.iloc[1:]
+
 
 #%%
-labels = dataset[0]
-data = dataset.drop(0,axis=1)
-
-#%%
-labels
-
-#%%
-data
-
-#%%
-scaled_data = scale(data)
-
-#%%
-scaled_data
-
-#%%
-#dataset = dataset.rename(columns={0 : "class", 1 : "left_weight", 2 : "left_distance", 3 : "right_weight", 4 :"right_distance"})
-#dataset["class"] = dataset["class"].astype('category')
-#dataset.head(10)
+dataset = dataset.rename(columns={0 : "class", 1 : "left_weight", 2 : "left_distance", 3 : "right_weight", 4 :"right_distance"})
+dataset["class"] = dataset["class"].astype('category')
+dataset.head(10)
 
 #%%
 dataset.dtypes
@@ -53,26 +40,26 @@ dataset.dtypes
 dataset.shape
 
 #%% [markdown]
-# 3. This data set was generated to model psychological experimental results. Each example is
-# classified as having the balance scale tip to the right, tip to the left, or be balanced. The
-# attributes are the left weight, the left distance, the right weight, and the right distance. The
-# correct way to find the class is the greater of (left-distance * left-weight) and (right-distance *
-# right-weight). If they are equal, it is balanced.
-#
+# 3. This data set was generated to model psychological experimental results.
+# Each example is classified as having the balance scale tip to the right, tip to the left, or be balanced.
+# The attributes are the left weight, the left distance, the right weight, and the right distance.
+# The correct way to find the class is the greater of:
+# (left-distance * left-weight) and (right-distance * # right-weight).
+# If they are equal, it is balanced.
 # Segment the outcome (first column) and remaining data (attributes) so we can use the attributes for clustering.
 #%%
-dataset_outcome = dataset.loc[:, 0]
+dataset_outcome = dataset.loc[:,"class"]
 dataset_outcome.head(10)
 
 #%%
-dataset_variables = dataset.loc[:, [1, 2, 3, 4]]
+dataset_variables = dataset.loc[:, ["left_weight", "left_distance", "right_weight", "right_distance"]]
 dataset_variables.head(10)
 
 #%%
 dataset_variables.describe()
 
 #%% [markdown]
-#4. Scale the data that we are going to use for clustering
+# 4. Scale the data that we are going to use for clustering
 #%%
 dataset_variables_scaled = pd.DataFrame(scale(dataset_variables))
 
@@ -80,17 +67,12 @@ dataset_variables_scaled = pd.DataFrame(scale(dataset_variables))
 dataset_variables_scaled.head(10)
 
 #%%
-dataset_variables_scaled.shape
-
-#%%
 dataset_variables_scaled.describe()
 
-
 #%% [markdown]
-#5. We know that there are 3 possible categories for the data. Create 3 data clusters using
-#Agglomerative Hierarchical Clustering. What are the silhouette score, homogeneity and
-#completeness for these clusters? (Helping hand, if you need to convert labels from strings to
-#something else look at sklearn.preprocessing.LabelEncoder())
+# 5. We know that there are 3 possible categories for the data. Create 3 data clusters using #Agglomerative Hierarchical Clustering.
+# What are the silhouette score, homogeneity and #completeness for these clusters?
+# (Helping hand, if you need to convert labels from strings to #something else look at sklearn.preprocessing.LabelEncoder())
 #%%
 n_samples, n_features = dataset_variables_scaled.shape
 print("n_samples: " + str(n_samples) + " - n_features: " + str(n_features))
@@ -107,26 +89,66 @@ model.fit(dataset_variables_scaled)
 
 
 #%% [markdown]
-#6. What are the impact of different distance and affinity measures on the silhouette score,
-#homogeneity and completeness for these clusters (options available at http://scikitlearn.org/stable/modules/generated/sklearn.cluster.AgglomerativeClustering.html)? What is the
-#best combination?
+# 6. What are the impact of different distance and affinity measures on the silhouette score, homogeneity and completeness for these clusters?
+# Options available at http://scikit-learn.org/stable/modules/generated/sklearn.cluster.AgglomerativeClustering.html
+# What is the best combination?
+#%%
+# To evaluate the performance of the model, we need to encode the L, B, R classification as integers..
+dataset_outcome_encoded = LabelEncoder().fit_transform(dataset_outcome)
+dataset_outcome_encoded
+
+#%%
+# Let's look at the labels that the model has assigned
+model.labels_
+
+#%% [markdown]
+# Now evaluate the performance of the culstering algorithm
+
+#%% [markdown]
+### Silhouette Score 
+# The Silhouette Coefficient is calculated using:
+# - the mean intra-cluster distance (a), and;
+# -  the mean nearest-cluster distance (b) for each sample.
+#
+#The Silhouette Coefficient for a sample is (b - a) / max(a, b).
+#
+# Advantages:
+# - The score is bounded between -1 for incorrect clustering and +1 for highly dense clustering. Scores around zero indicate overlapping clusters.
+# - The score is higher when clusters are dense and well separated, which relates to a standard concept of a cluster.
+
+#%%
+metrics.silhouette_score(dataset_variables_scaled, model.labels_)
+
+#%% [markdown]
+### Completeness Score
+# Completeness metric of a cluster labeling given a *ground truth*.
+# A clustering result satisfies completeness if all the data points that are members of a given class are elements of the same cluster.
+
+#%%
+metrics.completeness_score(dataset_outcome_encoded, model.labels_)
+
+#%% [markdown]
+### Homogeneity Score
+#Homogeneity metric of a cluster labeling given a ground truth.
+#A clustering result satisfies homogeneity if all of its clusters contain only data points which are members of a single class.
+
+#%%
+metrics.homogeneity_score(dataset_outcome_encoded, model.labels_)
+
+#%% [markdown]
+# 7. What are the silhouette score, homogeneity and completeness for different numbers of clusters created using KMeans?
 #%%
 
 #%% [markdown]
-7. What are the silhouette score, homogeneity and completeness for different numbers of clusters
-created using KMeans?
+# 8. Additional exercise. Read about Principle Component Analysis and look at this example -
+ # http://scikit-learn.org/stable/auto_examples/decomposition/plot_pca_iris.html Consider how it
+# could be applied to clustering in our examples.
 #%%
 
 #%% [markdown]
-8. Additional exercise. Read about Principle Component Analysis and look at this example -
-http://scikit-learn.org/stable/auto_examples/decomposition/plot_pca_iris.html Consider how it
-could be applied to clustering in our examples.
-#%%
-
-#%% [markdown]
-9. Using your interesting dataset that you have downloaded for analysis or example datasets from
-the lecture
-i. Use some of the techniques explored in the lecture try to examine your dataset
-ii. Reflect on your interesting question(s) and think about answering that question with
-these methods
-iii. Consider which of these methods (if any) might be good for answering your question(s)
+# 9. Using your interesting dataset that you have downloaded for analysis or example datasets from
+# the lecture
+# i. Use some of the techniques explored in the lecture try to examine your dataset
+# ii. Reflect on your interesting question(s) and think about answering that question with
+# these methods
+# iii. Consider which of these methods (if any) might be good for answering your question(s)
